01e555bead512157280028835505fbd8dde6f4d5
Valerie <valfragier@gmail.com>
2018-09-09 14:48:58 -0400

    updated

--- Assests/JavaScript/logic.js
+++ Assests/JavaScript/logic.js
@@ -1,5 +1,104 @@
+$(document).ready(function(event){
 
-var api = "http://api.giphy.com/v1/gifs/search";
-var apiKey = "&api_key=dc6zaT0xFJmzC";
-var query = "&q=famousWomen";
+	var women = ["Michelle Obama", "Ruth Bader Ginsburg", "Rosa Parks", "Chimamanda Ngozi Adichie", "Frida Kahlo"]; 
+	 
 
+	function search () {
+
+		var limits = 10 ; 
+		var key = "vYTrQMlbi3VzLIsdjs9Qs2a3BGUqMUXU" ; 
+		var search = $(this).attr("data-name");
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ search + "&api_key=" + key + "&limit=" + limits;
+
+		$(".giph").empty(); 
+
+		$.ajax({
+			url: queryURL, 
+			method: "GET"
+		}).done(function(response){
+			console.log(response); 
+			for (var j = 0 ; j < limits ; j++) {
+			
+				var holder = $("<div>");
+				holder.addClass("holders"); 
+				console.log(holder)
+				var stillImg = $("<img>").attr({
+					"src": response.data[j].images.fixed_height_still.url, 
+					"data-still": response.data[j].images.fixed_height_still.url, 
+					"data-animate": response.data[j].images.fixed_height.url, 
+					"data-state": "still", 
+					"class": "pics"
+				});
+			
+					console.log(stillImg); 
+
+				var ratings = $("<p>").text("Ratings: " + response.data[j].rating);
+					console.log(ratings)
+
+				$(holder).append(stillImg); 
+				$(holder).append(ratings); 
+
+				$(".giph").append(holder); 
+
+				loop(); 
+
+			}; 
+
+		}); 
+
+	}; 
+
+
+
+	function loop () {
+
+		$(".category").empty();
+
+		for (var i = 0 ; i < women.length ; i++) {	
+
+			var buttons = $("<button>");
+			buttons.text(women[i]); 
+			buttons.addClass("tabs");
+			buttons.attr("data-name", women[i])
+			$(".category").append(buttons);
+
+		};
+	};
+
+		$("#add-women").on("click", function (event) {
+
+			event.preventDefault();
+			var userAdd = $("#women-input").val().trim()
+			women.push(userAdd);  
+
+			
+			loop (); 
+
+		
+		}); 
+
+		function play () {
+		 	var state = $(this).attr("data-state"); 
+          		console.log("Initial State: " + state)
+        	var dataAnimate = $(this).attr("data-animate");
+        	var dataStill = $(this).attr("data-still"); 
+
+        	if (state === "still") {
+       			$(this).attr("src", dataAnimate); 
+        		$(this).attr("data-state", "animate"); 
+			 }
+
+			else  { 
+        		$(this).attr("src", dataStill); 
+        		$(this).attr("data-state", "still");
+      		}
+      	}; 
+
+		$(document).on("click", ".tabs", search); 
+		$(document).on("click", ".pics", play); 
+			
+		loop(); 
+		
+
+
+}); 
